f316dca8d4f1e57d3a544f04a4fb484156b2f1f2
njped <njpedigo@gmail.com>
2022-06-12 05:07:12 -0600

    finished the project

---
+++ app.js
@@ -0,0 +1,5 @@
+$('.navItem').css('color', 'white');
+$('#nav').css('background-color', '#145');
+$('li:odd').css('background-color', '#aaa');
+$('li:even').css('background-color', '#ccc');
+$('input[placeholder="First Name"').val("Nathan");
